f3f25a14a104683ed6a48eb7dc310c566fe476a3
pedrovmdp <pedromichelwork@gmail.com>
2025-09-17 21:55:05 -0300

    finished

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -10,7 +10,7 @@ import { Employer } from './employers/entities/employer.entity';
   imports: [EmployerModule,
     TypeOrmModule.forRoot({
       type: 'postgres',         
-      host: 'localhost',     
+      host: 'postgres',     
       port: 5432,            
       username: 'postgres',      
       password: 'postgres',     
